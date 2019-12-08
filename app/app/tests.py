f30bfe26a97bce8066ebddba700b9213cf9a8b07
Ganapati Bhat <bhatganapati6@gmail.com>
2019-12-08 19:20:46 +0530

    adding the django installations

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from app.calc import add
 
+
+
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """ tests that add two numbers """
-        self.assertEqual(add(3, 8), 11)
+        self.assertEqual(add(3, 1), 4)
         
