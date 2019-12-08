7541ac8b330c1779a6a40b220d3beed7f52d2dcd
Ganapati Bhat <bhatganapati6@gmail.com>
2019-12-08 19:06:46 +0530

    adding the app in settings

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,5 +4,5 @@ from app.calc import add
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """ tests that add two numbers """
-        self.assertEqual(add(3, 8), 1)
+        self.assertEqual(add(3, 8), 11)
         
